3a5f38f37f423976374913590a1e603d14ad1e13
isma132-web <wosansug100@gmail.com>
2026-03-28 15:15:31 -0300

    Create src/utils/web3Utils.ts file with Flashbots RPC and 1inch API integration functions

---
+++ src/utils/web3Utils.ts
@@ -0,0 +1,19 @@
+// web3Utils.ts
+
+/**
+ * Flashbots RPC Integration
+ */
+
+async function sendTransactionToFlashbots(signedTransaction) {
+    // Implementation for sending transaction to Flashbots
+}
+
+/**
+ * 1inch API Integration
+ */
+
+async function get1inchQuote(fromToken, toToken, amount) {
+    // Implementation for getting a quote from 1inch API
+}
+
+export { sendTransactionToFlashbots, get1inchQuote };
